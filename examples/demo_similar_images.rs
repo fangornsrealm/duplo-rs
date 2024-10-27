ea93c44f53d9818f6a720702d4670140d9f9b7c5
Alexander Schwarzkopf <Schwarzkopf.Alexander@web.de>
2024-10-27 11:58:31 +0100

    Image search working

--- examples/demo_similar_images.rs
+++ examples/demo_similar_images.rs
@@ -6,26 +6,25 @@ use simplelog::{ColorChoice, CombinedLogger, Config, TermLogger, TerminalMode, W
 use std::fs::File;
 
 /// Searches for similar images inside a directory or optionally also all it's subdirectories
-/// 
+///
 /// It can happen that it finds similarities that you will not see as such.
 /// If you want to decrease the sensitivity you can specify a value between 0 and 100 which will decrease the number of found similar images.
-/// 
-/// To enable you to check it creates a directory "duplicates" that contains pairs of images. 
+///
+/// To enable you to check it creates a directory "duplicates" that contains pairs of images.
 /// The file with the "KEEP" in the name will stay, as it is a hardlink (or a copy if your filesystem does not support hardlinks)
 /// The file with the "REMOVE" in the name will be gone if you delete it as it was moved there.
 /// They have the same random prefix so they are sorted together.
 /// The "better" of the two images was picked. The criteria for "better" could be more elaborate.
 /// Use any Image browser / file manager to review at your leisure.
 ///
-/// If you are satisfied with the selection, just delete all of the images. 
+/// If you are satisfied with the selection, just delete all of the images.
 /// If images were picked that you would like to keep, you have to move at least the files with "REMOVE" in the name somewhere else.
-/// 
+///
 pub fn main() {
     //ctrlc::set_handler(move || {
     //    crate::store::CTRL_C_PRESSED = true;
     //})
     //.expect("Error setting Ctrl-C handler");
-
 
     let mut logfile = "demo_similar_images.txt".to_string();
     let mut recursive = false;
@@ -77,13 +76,16 @@ pub fn main() {
     let dir = directory.clone();
     let p = std::path::Path::new(&dir);
     if !p.is_dir() {
-        log::error!("Directory {} does not exist. Specify an existing directory to search!", directory);
+        log::error!(
+            "Directory {} does not exist. Specify an existing directory to search!",
+            directory
+        );
         std::process::exit(1);
     }
     // create the directory where the user can compare the similar image pairs
-    let dst: std::path::PathBuf = p.join("duplicates");
+    let dst: std::path::PathBuf = p.join("similar_images");
     let storepath = p.join("demo_example_images.store");
-    let mut store: duplo_rs::store::Store = duplo_rs::store::Store::new(sensitivity);;
+    let mut store: duplo_rs::store::Store = duplo_rs::store::Store::new(sensitivity);
     if storepath.is_file() {
         let storefile = duplo_rs::files::osstring_to_string(storepath.as_os_str());
         store.slurp_binary(&storefile);
@@ -103,7 +105,7 @@ pub fn main() {
     for file in filelist.iter() {
         let hash = duplo_rs::files::process_image(file);
         let filepath = duplo_rs::files::osstring_to_string(file.as_os_str());
-        let (matches, failedid, failedhash) = 
+        let (matches, failedid, failedhash) =
             duplo_rs::files::find_similar_images(&store, &filepath, &hash);
         progressbar.inc();
         for i in 0..matches.m.len() {
--- src/haar.rs
+++ src/haar.rs
@@ -1,5 +1,4 @@
 /// Package haar provides a Haar wavelet function for bitmap images.
-
 use image;
 use image::Pixel;
 
@@ -31,12 +30,16 @@ impl Coef {
     }
 
     pub fn new() -> Self {
-        let v = Coef {..Default::default()};
+        let v = Coef {
+            ..Default::default()
+        };
         v
     }
 
     pub fn from(r: f64, g: f64, b: f64) -> Self {
-        let mut v = Coef{..Default::default()};
+        let mut v = Coef {
+            ..Default::default()
+        };
         v.c[0] += r;
         v.c[1] += g;
         v.c[2] += b;
@@ -55,8 +58,8 @@ impl Coef {
 impl Default for Coef {
     fn default() -> Coef {
         Coef {
-            c: Vec::from([0.0;COLOURCHANNELS as usize]),
-       }
+            c: Vec::from([0.0; COLOURCHANNELS as usize]),
+        }
     }
 }
 
@@ -69,7 +72,9 @@ pub struct CoefMatrix {
 
 impl CoefMatrix {
     pub fn new() -> Self {
-        let v = CoefMatrix{..Default::default()};
+        let v = CoefMatrix {
+            ..Default::default()
+        };
         v
     }
 
@@ -103,7 +108,7 @@ impl Default for CoefMatrix {
             coefs: Vec::new(),
             width: 0,
             height: 0,
-       }
+        }
     }
 }
 
@@ -112,11 +117,13 @@ impl Default for CoefMatrix {
 /// the store is built to handle different sized Coef's so any length may be
 /// returned.)
 pub fn color_to_coef(color: &image::Rgba<u8>) -> Coef {
-    let mut coef = Coef{..Default::default()};
+    let mut coef = Coef {
+        ..Default::default()
+    };
 
     // Convert into YIQ. (We may want to convert from YCbCr directly one day.)
     //color.
-	//r32, g32, b32, _ := gen.RGBA()
+    //r32, g32, b32, _ := gen.RGBA()
     let rgb = Vec::from(color.channels());
     let ru = rgb[0] as usize;
     let r = ru as f64;
@@ -124,16 +131,18 @@ pub fn color_to_coef(color: &image::Rgba<u8>) -> Coef {
     let g = gu as f64;
     let bu = rgb[2] as usize;
     let b = bu as f64;
-	//r, g, b := float64(r32>>8), float64(g32>>8), float64(b32>>8)
-	
-	coef.c[0] = (0.299900*r + 0.587000*g + 0.114000*b) / 0x100 as f64;
-	coef.c[1] = (0.595716*r - 0.274453*g - 0.321263*b) / 0x100 as f64;
-	coef.c[2] = (0.211456*r - 0.522591*g + 0.311135*b) / 0x100 as f64;
+    //r, g, b := float64(r32>>8), float64(g32>>8), float64(b32>>8)
+
+    coef.c[0] = (0.299900 * r + 0.587000 * g + 0.114000 * b) / 0x100 as f64;
+    coef.c[1] = (0.595716 * r - 0.274453 * g - 0.321263 * b) / 0x100 as f64;
+    coef.c[2] = (0.211456 * r - 0.522591 * g + 0.311135 * b) / 0x100 as f64;
     coef
 }
 
 pub fn transform(img: &image::RgbaImage) -> CoefMatrix {
-    let mut matrix = CoefMatrix{..Default::default()};
+    let mut matrix = CoefMatrix {
+        ..Default::default()
+    };
     let mut width = img.width();
     let mut height = img.height();
     if width % 2 != 0 {
@@ -142,7 +151,7 @@ pub fn transform(img: &image::RgbaImage) -> CoefMatrix {
     if height % 2 != 0 {
         height = height | 1;
     }
-    let reservelen :usize = width as usize * height as usize;
+    let reservelen: usize = width as usize * height as usize;
     for _ in 0..reservelen {
         matrix.coefs.push(Coef::new());
     }
@@ -150,64 +159,68 @@ pub fn transform(img: &image::RgbaImage) -> CoefMatrix {
     matrix.height = height;
 
     // Convert colours to coefficients.
-	for row in 0..height as usize {
-		for column in 0..width as usize {
-			matrix.coefs[row*width as usize +column] = 
+    for row in 0..height as usize {
+        for column in 0..width as usize {
+            matrix.coefs[row * width as usize + column] =
                 color_to_coef(img.get_pixel(column as u32, row as u32));
-		}
-	}
+        }
+    }
 
-	// Apply 1D Haar transform on rows.
-	let mut temp_row = Vec::new();
+    // Apply 1D Haar transform on rows.
+    let mut temp_row = Vec::new();
     for _ in 0..width {
-        temp_row.push(Coef {..Default::default()});
+        temp_row.push(Coef {
+            ..Default::default()
+        });
     }
-	for row in 0..height {
-        let mut step = width/2;
-		while step >= 1 {
-			for column in 0..step {
-				let mut high = matrix.coefs[(row*width+2*column) as usize].clone();
-				let mut low = high.clone();
-				let offset = matrix.coefs[(row*width+2*column+1) as usize].clone();
-				high.add(offset.clone());
-				low.subtract(offset.clone());
-				high.divide(2.0_f64.sqrt());
-				low.divide(2.0_f64.sqrt());
-				temp_row[column as usize] = high;
-				temp_row[(column+step) as usize] = low;
-			}
-			for column in 0..width {
-				matrix.coefs[(row*width+column) as usize] = temp_row[column as usize].clone();
-			}
+    for row in 0..height {
+        let mut step = width / 2;
+        while step >= 1 {
+            for column in 0..step {
+                let mut high = matrix.coefs[(row * width + 2 * column) as usize].clone();
+                let mut low = high.clone();
+                let offset = matrix.coefs[(row * width + 2 * column + 1) as usize].clone();
+                high.add(offset.clone());
+                low.subtract(offset.clone());
+                high.divide(2.0_f64.sqrt());
+                low.divide(2.0_f64.sqrt());
+                temp_row[column as usize] = high;
+                temp_row[(column + step) as usize] = low;
+            }
+            for column in 0..width {
+                matrix.coefs[(row * width + column) as usize] = temp_row[column as usize].clone();
+            }
             step /= 2;
-		}
-	}
+        }
+    }
 
-	// Apply 1D Haar transform on columns.
-	let mut temp_column = Vec::new();
+    // Apply 1D Haar transform on columns.
+    let mut temp_column = Vec::new();
     for _ in 0..height {
-        temp_column.push(Coef {..Default::default()});
+        temp_column.push(Coef {
+            ..Default::default()
+        });
     }
-	for column in 0..width {
-        let mut step = height/2;
-		while step >= 1 {
-			for row in 0..step {
-				let mut high = matrix.coefs[(2*row*width+column) as usize].clone();
-				let mut low = high.clone();
-				let offset = matrix.coefs[((2*row+1)*width+column) as usize].clone();
-				high.add(offset.clone());
-				low.subtract(offset.clone());
-				high.divide(2.0_f64.sqrt());
-				low.divide(2.0_f64.sqrt());
-				temp_column[row as usize] = high;
-				temp_column[(row+step) as usize] = low;
-			}
-			for row in 0..height {
-				matrix.coefs[(row*width+column) as usize] = temp_column[row as usize].clone();
-			}
+    for column in 0..width {
+        let mut step = height / 2;
+        while step >= 1 {
+            for row in 0..step {
+                let mut high = matrix.coefs[(2 * row * width + column) as usize].clone();
+                let mut low = high.clone();
+                let offset = matrix.coefs[((2 * row + 1) * width + column) as usize].clone();
+                high.add(offset.clone());
+                low.subtract(offset.clone());
+                high.divide(2.0_f64.sqrt());
+                low.divide(2.0_f64.sqrt());
+                temp_column[row as usize] = high;
+                temp_column[(row + step) as usize] = low;
+            }
+            for row in 0..height {
+                matrix.coefs[(row * width + column) as usize] = temp_column[row as usize].clone();
+            }
             step /= 2;
-		}
-	}
+        }
+    }
 
     matrix
 }
@@ -216,86 +229,89 @@ pub fn transform(img: &image::RgbaImage) -> CoefMatrix {
 
 // Whether or not the two coefficients are equal to an epsilon difference.
 fn equal(slice1: &Coef, slice2: &Coef) -> bool {
-	for index in 0..slice1.c.len() {
-		if (slice1.c[index]-slice2.c[index]).abs() > 0.002 {
-			return false
-		}
-	}
-	return true
+    for index in 0..slice1.c.len() {
+        if (slice1.c[index] - slice2.c[index]).abs() > 0.002 {
+            return false;
+        }
+    }
+    return true;
 }
 
 // Whether or not the two matrices are equal (uses equal() function).
-fn equal_matrices(matrix1: &CoefMatrix, matrix2: &CoefMatrix) -> bool {
-	if matrix1.width != matrix2.width {
-		return false;
-	}
-	if matrix1.height != matrix2.height {
-		return false;
-	}
-	if matrix1.coefs.len() != matrix2.coefs.len() {
-		return false;
-	}
-	for index in 0..matrix1.coefs.len() {
+fn _equal_matrices(matrix1: &CoefMatrix, matrix2: &CoefMatrix) -> bool {
+    if matrix1.width != matrix2.width {
+        return false;
+    }
+    if matrix1.height != matrix2.height {
+        return false;
+    }
+    if matrix1.coefs.len() != matrix2.coefs.len() {
+        return false;
+    }
+    for index in 0..matrix1.coefs.len() {
         if matrix1.coefs[index].c.len() != matrix2.coefs[index].c.len() {
             return false;
         }
         for coefid in 0..matrix1.coefs[index].c.len() {
-            if (matrix1.coefs[index].c[coefid]-matrix2.coefs[index].c[coefid]) > 0.002 {
+            if (matrix1.coefs[index].c[coefid] - matrix2.coefs[index].c[coefid]) > 0.002 {
                 return false;
             }
         }
-	}
-	true
+    }
+    true
 }
 
 // Converts a slice of floats to a Coefs slice as found in a one-value matrix.
-fn floats_to_coefs(floats: &Vec<f64>) -> CoefMatrix {
-	let mut coefs = CoefMatrix::new();
-	for index in 0..floats.len() {
-		coefs.coefs.push(Coef::from(floats[index], 0.0, 0.0));
-	}
-	coefs
+fn _floats_to_coefs(floats: &Vec<f64>) -> CoefMatrix {
+    let mut coefs = CoefMatrix::new();
+    for index in 0..floats.len() {
+        coefs.coefs.push(Coef::from(floats[index], 0.0, 0.0));
+    }
+    coefs
 }
 
 // Test coefficients.
 #[test]
 fn test_coef() {
-	let mut coef = Coef::from(1.0, 2.0, 3.0);
-	let copy_coef = coef.clone();
-	assert_eq!(equal(&copy_coef, &Coef::from(1.0, 2.0, 3.0)), true);
+    let mut coef = Coef::from(1.0, 2.0, 3.0);
+    let copy_coef = coef.clone();
+    assert_eq!(equal(&copy_coef, &Coef::from(1.0, 2.0, 3.0)), true);
 
-	let offset = Coef::from(2.0, 4.0, 6.0);
-	coef.add(offset.clone());
-	assert_eq!(equal(&coef, &Coef::from(3.0, 6.0, 9.0)), true);
+    let offset = Coef::from(2.0, 4.0, 6.0);
+    coef.add(offset.clone());
+    assert_eq!(equal(&coef, &Coef::from(3.0, 6.0, 9.0)), true);
 
-	coef.subtract(offset);
-	assert_eq!(equal(&coef, &Coef::from(1.0, 2.0, 3.0)), true);
+    coef.subtract(offset);
+    assert_eq!(equal(&coef, &Coef::from(1.0, 2.0, 3.0)), true);
 
-	coef.divide(2.0);
-	assert_eq!(equal(&coef, &Coef::from(0.5, 1.0, 1.5)), true);
+    coef.divide(2.0);
+    assert_eq!(equal(&coef, &Coef::from(0.5, 1.0, 1.5)), true);
 }
 
 // Test the proper RGB-YIQ conversion.
 #[test]
 fn test_color_conversion() {
-	let rgb = image::Rgba::from([64_u8, 0, 128, 255]);
-	let coef = color_to_coef(&rgb);
-	assert_eq!(equal(&coef, &Coef::from(0.131975, -0.0117025, 0.2084315)), true);
+    let rgb = image::Rgba::from([64_u8, 0, 128, 255]);
+    let coef = color_to_coef(&rgb);
+    assert_eq!(
+        equal(&coef, &Coef::from(0.131975, -0.0117025, 0.2084315)),
+        true
+    );
 }
 
 // Essentially a 1D Haar Wavelet test.
 #[test]
 fn test_single_row() {
-	// This is a rough approximation to a 4px by 1px YIQ image with pixels
-	// .04, .02, .05, .05. Y, I, and Q all have the same value.
+    // This is a rough approximation to a 4px by 1px YIQ image with pixels
+    // .04, .02, .05, .05. Y, I, and Q all have the same value.
     let ret = image::load_from_memory(&[26_u8, 1, 16, 1, 13, 0, 8, 1, 33, 1, 20, 1, 33, 1, 20, 1]);
-	if ret.is_ok() {
+    if ret.is_ok() {
         let input = ret.unwrap();
         let transopt = input.as_rgba8();
         if transopt.is_some() {
             let output = transform(transopt.unwrap());
-            let expected = floats_to_coefs(&vec!(0.08, -0.02, 0.02 / 2.0_f64.sqrt(), 0.0));
-            assert_eq!(equal_matrices(&output, &expected), true);            
+            let expected = _floats_to_coefs(&vec![0.08, -0.02, 0.02 / 2.0_f64.sqrt(), 0.0]);
+            assert_eq!(_equal_matrices(&output, &expected), true);
         }
     }
 }
@@ -303,23 +319,38 @@ fn test_single_row() {
 // Basic 2D Haar Wavelet test.
 #[test]
 fn test_matrix4x4() {
-	// This is a rough approximation to a 4px by 4px YIQ image with consecutive
-	// pixels increasing by one each (.01, .02, .03, .04, ..., .16) and Y, I, and
-	// Q having the same values.
-    let ret = image::load_from_memory(
-        &[7, 0, 4, 1, 13, 0, 8, 1, 20, 1, 12, 1, 26, 1, 16, 1,
-                33, 1, 20, 1, 39, 1, 24, 1, 46, 1, 29, 1, 53, 2, 33, 1,
-                59, 2, 37, 1, 66, 2, 41, 1, 72, 2, 45, 1, 79, 2, 49, 1,
-                85, 3, 53, 1, 92, 3, 57, 1, 99, 3, 61, 1, 105, 3, 65, 1]);
-	if ret.is_ok() {
+    // This is a rough approximation to a 4px by 4px YIQ image with consecutive
+    // pixels increasing by one each (.01, .02, .03, .04, ..., .16) and Y, I, and
+    // Q having the same values.
+    let ret = image::load_from_memory(&[
+        7, 0, 4, 1, 13, 0, 8, 1, 20, 1, 12, 1, 26, 1, 16, 1, 33, 1, 20, 1, 39, 1, 24, 1, 46, 1, 29,
+        1, 53, 2, 33, 1, 59, 2, 37, 1, 66, 2, 41, 1, 72, 2, 45, 1, 79, 2, 49, 1, 85, 3, 53, 1, 92,
+        3, 57, 1, 99, 3, 61, 1, 105, 3, 65, 1,
+    ]);
+    if ret.is_ok() {
         let input = ret.unwrap();
         let transopt = input.as_rgba8();
         if transopt.is_some() {
             let output = transform(transopt.unwrap());
-            let expected = floats_to_coefs(&vec!(0.34_f64, -0.04, -2.0_f64.sqrt() / 100.0, 0.0,
-            -0.16, 0.0, 0.0, 0.0, -0.04 * 2.0_f64.sqrt(), 
-            0.0, 0.0, 0.0,-0.04 * 2.0_f64.sqrt(), 0.0, 0.0, 0.0));
-            assert_eq!(equal_matrices(&output, &expected), true);
+            let expected = _floats_to_coefs(&vec![
+                0.34_f64,
+                -0.04,
+                -2.0_f64.sqrt() / 100.0,
+                0.0,
+                -0.16,
+                0.0,
+                0.0,
+                0.0,
+                -0.04 * 2.0_f64.sqrt(),
+                0.0,
+                0.0,
+                0.0,
+                -0.04 * 2.0_f64.sqrt(),
+                0.0,
+                0.0,
+                0.0,
+            ]);
+            assert_eq!(_equal_matrices(&output, &expected), true);
         }
     }
 }
